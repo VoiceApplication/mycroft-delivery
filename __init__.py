f5f00fa32ef57551f65ff9e8d9f9e7b5fc4083b5
twming <tanwoeiming@gmail.com>
2021-04-18 00:24:38 +0800

    update

--- __init__.py
+++ __init__.py
@@ -22,16 +22,18 @@ import json
 import random
 
 
-class LoadProject(MycroftSkill):
+class DeliveryProject(MycroftSkill):
     def __init__(self):
         """ The __init__ method is called when the Skill is first constructed.
         It is often used to declare variables or perform setup actions, however
         it cannot utilise MycroftSkill methods as the class does not yet exist.
         """
         super().__init__()
-        self.url_get='http://42.60.37.128:5000/iss/delivery/address/5/761512'
-        self.url_resolve_address='https://developers.onemap.sg/commonapi/search?searchVal=761512&returnGeom=N&getAddrDetails=Y'
+        self.url_start='http://42.60.37.128:5000/iss/delivery/address/5/761512'
+        self.url_next='http://42.60.37.128:5000/iss/delivery/address/5/761512'
+        self.url_resolve_address='https://developers.onemap.sg/commonapi/search?searchVal='
         self.current_postcode=761512
+        self.current_location=''
         self.remain_package=0
 
     def initialize(self):
@@ -41,23 +43,48 @@ class LoadProject(MycroftSkill):
         settings will be available."""
         my_setting = self.settings.get('my_setting')
 
-    @intent_handler(IntentBuilder('askIntent').require('ask'))
-    def handle_ask_intent(self, message):
-        r=requests.get(self.url_get)
+
+    @intent_handler(IntentBuilder('startIntent').require('start'))
+    def handle_start_intent(self, message):
+        r=requests.get(self.url_start)
         if r.status_code==200:
             myadd=[int(s) for s in r.text.split() if s.isdigit()]
             self.current_postcode=myadd[1]
             self.remain_package=myadd[0]
-            r1=requests.get(self.url_resolve_address)
+            r1=requests.get(self.url_resolve_address+str(myadd[1])+'&returnGeom=N&getAddrDetails=Y')
             if r1.status_code==200:
                 data=json.loads(r1.text)
-                self.log.info('Vehicle 5. Your next delivery point is '+myadd[1]+' '+data['results'][0]['SEARCHVAL'])
+                self.current_location=data['results'][0]['SEARCHVAL']
+                self.speak_dialog('Vehicle 5. Your next delivery point is '+str(myadd[1])+' '+data['results'][0]['SEARCHVAL'])
+                self.log.info('Vehicle 5. Your next delivery point is '+str(myadd[1])+' '+data['results'][0]['SEARCHVAL'])
+                self.url_next='http://42.60.37.128:5000/iss/delivery/address/5/'+str(myadd[1])
+        else:
+            self.speak_dialog('Sorry, you have no package to deliver')
+
+
+    @intent_handler(IntentBuilder('nextIntent').require('next'))
+    def handle_ask_intent(self, message):
+        r=requests.get(self.url_next)
+        if r.status_code==200:
+            myadd=[int(s) for s in r.text.split() if s.isdigit()]
+            if len(myadd)!=0:
+                self.current_postcode=myadd[1]
+                self.remain_package=myadd[0]
+                r1=requests.get(self.url_resolve_address+str(myadd[1])+'&returnGeom=N&getAddrDetails=Y')
+                if r1.status_code==200:
+                    data=json.loads(r1.text)
+                    self.current_location=data['results'][0]['SEARCHVAL']
+                    self.speak_dialog('Vehicle 5. Your next delivery point is '+str(myadd[1])+' '+data['results'][0]['SEARCHVAL'])
+                    self.log.info('Vehicle 5. Your next delivery point is '+str(myadd[1])+' '+data['results'][0]['SEARCHVAL'])
+                    self.url_next='http://42.60.37.128:5000/iss/delivery/address/5/'+str(myadd[1])
+            else:
+                self.speak_dialog('You complete the delivery')
         else:
             self.speak_dialog('Sorry, you have no package to deliver')
 
     @intent_handler(IntentBuilder('checkIntent').require('check'))
-    def handle_ans_intent(self, message):
-        self.speak_dialog('You have '+remain_package+' package to deliver')
+    def handle_check_intent(self, message):
+        self.speak_dialog('You have '+str(self.remain_package)+' package to deliver, current at '+self.current_location)
 
 
     def stop(self):
@@ -65,4 +92,4 @@ class LoadProject(MycroftSkill):
 
 
 def create_skill():
-    return LoadProject()
+    return DeliveryProject()
